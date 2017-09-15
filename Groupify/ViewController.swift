35d992c245d95ba025a927eb64e521a3da9f9a40
Andrew Dobroshynskyi <adobro.developer@gmail.com>
2017-09-15 02:21:31 -0400

    added a new modal view controller and group slider picker work as intended

--- Groupify/ViewController.swift
+++ Groupify/ViewController.swift
@@ -23,10 +23,28 @@ class ViewController: UIViewController {
     }
     
     @IBAction func groupSizeSlideChanged(_ sender: Any) {
-        
+        groupSizePicker.value = roundf((groupSizePicker.value));
     }
     
     @IBAction func findGroupButtonPressed(_ sender: Any) {
+        let groupTypeNumber: Float = roundf((groupSizePicker.value));
+        var groupType : String;
+        
+        switch groupTypeNumber {
+        case 1.0:
+            groupType = "S"
+        case 2.0:
+            groupType = "M"
+        case 3.0:
+            groupType = "L"
+        default:
+            groupType = "XL"
+        }
+        // make a call to pick a random group; type of group chosen in in groupType
+        
+    }
+    
+    @IBAction func unwindToHomeVC(segue:UIStoryboardSegue) {
         
     }
 }
