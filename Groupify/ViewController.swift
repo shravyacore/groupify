b9ecb98eec22ef05d1601f25f71b92667efb9d86
Andrew Dobroshynskyi <adobro.developer@gmail.com>
2017-09-15 01:05:31 -0400

    added some UI, changed deployment target

--- Groupify/ViewController.swift
+++ Groupify/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var groupSizePicker: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func groupSizeSlideChanged(_ sender: Any) {
+        
+    }
+    
+    @IBAction func findGroupButtonPressed(_ sender: Any) {
+        
+    }
 }
 
